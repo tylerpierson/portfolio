39a3cc0456362c9dba2ad6ebf4a6b9a7d67ec450
Tyler Pierson <tylerpierson92@gmail.com>
2024-03-29 17:28:24 -0500

    Changed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('./config/database')
 
 const app = require('./app-server')
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8002
 
 app.listen(PORT, () => {
 	console.log(`App is listening on ${PORT}.`)
